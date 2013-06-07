0532540098672fcf19bfc069bf4befabd8eb7c84
Yannick Mauray <yannick.mauray@gmail.com>
2013-06-07 07:34:35 -0700

    Initial commit

---
+++ erpress3.php
@@ -0,0 +1,27 @@
+<?php
+/*
+Plugin Name: erpress3
+Plugin URI: http://www.euterpia-radio.fr/plugins/erpress3/
+Description: Plugin to manage show notes.
+Version: 3.0.0 (Cheerfull Cougar)
+Author: Yannick
+Author URI: http://www.euterpia-radio.fr/
+Contributors:
+Yannick
+
+Credits:
+
+Copyright 2013 - Euterpia Radio
+
+License: GPL (http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt)
+*/
+
+class WPF2Plugin {
+}
+
+class ERPress3 extends WPF2Plugin {
+}
+
+new ERPress3();
+
+?>
